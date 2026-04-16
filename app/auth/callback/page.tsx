455c1ce1a9d2cdc0c7aba1b6a7c86d64e352fcc8
vikramprv-blip <vikramprv@gmail.com>
2026-04-16 21:57:50 +0530

    Fix use client order for Next.js

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,5 +1,5 @@
+"use client";
 export const dynamic = "force-dynamic";
-"use client"
 import { useEffect } from "react"
 import { useSearchParams } from "next/navigation"
 import { createClient } from "../../../lib/supabase-browser"
