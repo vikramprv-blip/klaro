339043956ad5ac42b5be309a7f9d9ce00f5f2838
vikramch-7755 <vikramch@hotmail.com>
2026-04-28 19:46:24 +0530

    Remove generic EU route and prepare country-first region selector

---
+++ app/in/layout.tsx
@@ -0,0 +1,7 @@
+export default function IndiaLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
---
+++ app/vn/layout.tsx
@@ -0,0 +1,7 @@
+export default function RegionalLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
